185044abd53eb046243c4f7f7c5d0a0a986fe590
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2020-05-13 23:39:43 -0400

    Edited Customer model defination
    It was irrelevant to have cart as a fild of customer. Cart can be recognize by its user field

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from store.models import Cart
+from store.models import Product
 
 # Create your models here.
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    favorit = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.user.email
