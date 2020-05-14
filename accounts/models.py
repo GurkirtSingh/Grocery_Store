450ffecf55a82724e1f38f98dc5e419bbfbdb622
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2020-05-13 22:22:16 -0400

    customer model is created
    By extending User model, Customer model is created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from store.models import Cart
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
