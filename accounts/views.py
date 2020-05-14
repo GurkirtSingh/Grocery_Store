72cd4e36c150f5e1e8d7d8b17267a1f97b8e06c6
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2020-05-14 01:08:18 -0400

    created functions to handle login and signup request
    loginUser and signupUser function still needed to add functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
+def loginUser(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+
+def signupUser(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/signup.html')
