772a31d8766b092fb4dd6d29509a2c3e6f771c01
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2020-05-14 01:06:05 -0400

    Create urls.py
    This file take care of routing account related queries to the views

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import loginUser, signupUser
+urlpatterns = [
+    path('login/', loginUser, name = 'login'),
+    path('signup/', signupUser, name = 'signup'),
+]
