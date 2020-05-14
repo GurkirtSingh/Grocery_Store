55f77e0cba9a03b3484b726d49e9f87f9d950185
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2020-05-13 23:41:36 -0400

    Models get register by admin
    Customer, Department, Product and Cart gets register in admin site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from accounts.models import Customer
+from store.models import Department, Product, Cart
 
 # Register your models here.
+admin.site.register([Customer, Department, Product, Cart])
