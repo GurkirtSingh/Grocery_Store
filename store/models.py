68b36d44d4446baaefcf509534b8d9e07c06705c
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2020-05-13 22:21:28 -0400

    store models are created
    Created models for Department, Product and Cart

--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# a model for grouping the products
+class Department(models.Model):
+    name = models.CharField()
+    
+
+# a model to describe product item
+class Product(models.Model):
+    name = models.CharField()
+    price = models.DecimalField(max_digits=2)
+    desc = models.CharField()
+    department = models.ManyToManyField(Department)
+    priceRate = models.DecimalField(max_digits=2)
+    image = models.ImageField()
+    quantityType = models.CharField()
+
+
+# a model for cart item
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    subTotal = models.DecimalField(max_digits=2)
+    totalTax = models.DecimalField(max_digits=2)
+    total = models.DecimalField(max_digits=2)
